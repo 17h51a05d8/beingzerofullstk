74bf22941eb598b088ca1d5866fa68c508360984
17h51a05d8 <aishwaryagumastha18@gmail.com>
2020-01-02 12:58:34 +0530

    added nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { PrincingComponent } from './princing/princing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FeaturesComponent,
+    PrincingComponent
   ],
   imports: [
     BrowserModule,
