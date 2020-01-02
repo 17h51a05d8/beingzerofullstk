9fd5fb8585be4d25796ec377b97ef3e1ec6b4c5d
17h51a05d8 <aishwaryagumastha18@gmail.com>
2020-01-02 17:38:07 +0530

    created app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { PrincingComponent } from './princing/princing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'features',component:FeaturesComponent},
+  {path:'pricing',component:PrincingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/princing/princing.component.ts
+++ src/app/princing/princing.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-princing',
   templateUrl: './princing.component.html',
   styleUrls: ['./princing.component.css']
 })
 export class PrincingComponent implements OnInit {
-
-  constructor() { }
+   users= {};
+   count=0;
+  constructor(private data:DataService) { }
 
   ngOnInit() {
-  }
+    this.data.getData().subscribe(d => {
+    this.users['arr']=d;
+    //alert(this.users.arr.length);
+    this.count=this.users['arr'].length;
+  })
+}
 
 }
